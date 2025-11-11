ae3bec446fafafa2e2998d48da766f97869a4f0a
rh-ralib04 <rakibulhossainbhuiya04@.com>
2025-11-11 12:55:53 +0600

    added cars data

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-import express from "express";
-import cors from "cors";
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://rent-wheel:CqKOYb2cekAPEfh8@cluster0.aczt7zj.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to RentWheels API ");
 });
 
-const PORT = process.env.PORT || 5000;
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("rent-wheel");
+    const carsCollection = db.collection("cars");
+
+    // ------------cars api----------
+
+    app.get("/cars", async (req, res) => {
+      const result = await carsCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
